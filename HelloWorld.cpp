bc0e3d8899f1364eee5ce1a6d82fabee8ee6c271
saquiba2 <swtariq@ucdavis.edu>
2015-10-31 19:56:25 -0700

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -14,7 +14,9 @@ int main (){
 
 	cout<<"hello world! var is: "<<var << "and arr[1]is: " << arr[1] <<endl;
 	cout<<"This is the third of a series of 3 commits before sending a pull request." <<endl;
-	cout<<"now this branch is many commits behind the saquiba original branch";
+
+
+	cout<<"now this branch, saquiba2's, is many commits behind the saquiba original branch. this is somethign added additionally though.";
 	
 	return 0;
 }
